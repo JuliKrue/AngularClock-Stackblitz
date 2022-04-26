2f25887db819aa837713da5198bd16805cb5d54e
JuliKrue <juli.krueger@outlook.de>
2022-04-26 13:14:05 +0200

    feat(Clock): Added Useless facts API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UselessFactsApiService } from './uselessfacts-api.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent, HelloComponent, ButtonComponent],
+  providers: [UselessFactsApiService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
