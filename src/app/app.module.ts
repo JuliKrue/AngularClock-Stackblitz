56b3772028baf8c196a65d83708163cf74f71303
JuliKrue <juli.krueger@outlook.de>
2022-05-04 11:12:45 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { Hello Component} from './hello.component';
+import { HelloComponent } from './hello.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UselessFactsApiService } from './uselessfacts-api.service';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  declarations: [AppComponent, HelloComponent, ButtonComponent],
+  declarations: [AppComponent, HelloComponent],
   providers: [UselessFactsApiService],
   bootstrap: [AppComponent],
 })
