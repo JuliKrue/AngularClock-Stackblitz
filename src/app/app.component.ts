9e4a94e0f4dc78a25be0c63cddd35af89413569f
JuliKrue <juli.krueger@outlook.de>
2022-05-05 11:46:16 +0200

    feat(app.component.ts): Added Clock function on init, Added clock to app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  clockdata = ""
+  clockHandle;
+ 
+
+  ngOnInit(){
+    this.clockHandle = setInterval(()=> {
+      this.clockdata = new Date().toLocaleString();
+    }, 1000);
+  }
 }
--- src/app/uselessfacts-api.service.ts
+++ src/app/uselessfacts-api.service.ts
@@ -4,4 +4,8 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class UselessFactsApiService {
   constructor(private http: HttpClient) {}
+
+  getRandom(){
+    return this.http.get('https://uselessfacts.jsph.pl/');
+  }
 }
